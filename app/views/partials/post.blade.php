e60e1287ff8d8e00061a6a4125a0d84b7e2d901a
Adam Wathan <adam.wathan@gmail.com>
2014-01-06 22:35:58 -0500

    Show real date instead of hardcoded August-2013 like an asshat

--- app/views/partials/post.blade.php
+++ app/views/partials/post.blade.php
@@ -1,5 +1,5 @@
 <article>
-	<div class="published"><span class="date">August 23, 2013</span></div>
+	<div class="published"><span class="date">{{ (new DateTime($post->date))->format('F j, Y') }}</span></div>
 	<header>
 		<h1>{{ link_to_post($post) }}</h1>
 		<div class="byline">by {{ $post->author }}</div>
