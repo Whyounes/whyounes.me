ba3f173ed4533628a0444263134e6f01a1451b7e
Adam Wathan <adam.wathan@gmail.com>
2014-10-19 11:47:22 -0400

    Update about page

--- app/views/about.blade.php
+++ app/views/about.blade.php
@@ -3,8 +3,8 @@
 @section('content')
 <div class="about">
 	<h1>About</h1>
-	<p>I build web applications at <a href="http://vehikl.com">Vehikl</a> in Waterloo by day, and <a href="http://www.youtube.com/watch?v=2-J-DCbsQzo">lift heavy things competitively</a> by night.</p>
+	<p>I build web applications at <a href="http://boltmade.com">Boltmade</a> in Waterloo by day, and <a href="http://www.youtube.com/watch?v=2-J-DCbsQzo">lift heavy things competitively</a> by night.</p>
 
-	<p>You can check me out on <a href="https://github.com/adamwathan">Github</a>, follow me on <a href="http://twitter.com/adamwathan">Twitter</a> or email me at <a href="mailto:adam.wathan@gmail.com">adam.wathan@gmail.com</a>.</p>
+	<p>You can check me out on <a href="https://github.com/adamwathan">GitHub</a>, follow me on <a href="http://twitter.com/adamwathan">Twitter</a> or email me at <a href="mailto:adam.wathan@gmail.com">adam.wathan@gmail.com</a>.</p>
 </div>
 @stop
