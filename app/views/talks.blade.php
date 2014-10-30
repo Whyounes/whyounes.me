f3cb1b1b8136e132e54a60189b4d36804c5b74b3
Adam Wathan <adam.wathan@gmail.com>
2014-10-30 09:29:31 -0400

    Responsive styling for embedded videos

--- app/views/talks.blade.php
+++ app/views/talks.blade.php
@@ -4,8 +4,8 @@
 <div class="content-section">
 	<h1>Talks</h1>
 
-    <div class="text-center">
-        <iframe src="//player.vimeo.com/video/110388553" width="500" height="375" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+    <div class="talk-video">
+        <iframe src="//player.vimeo.com/video/110388553" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
     </div>
 </div>
 @stop
