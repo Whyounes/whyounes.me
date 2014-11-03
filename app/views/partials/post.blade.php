9e9e143107dd96960da9b93ea39396eb53fa727d
Adam Wathan <adam.wathan@gmail.com>
2014-11-03 13:32:40 -0500

    Update styles

--- app/views/partials/post.blade.php
+++ app/views/partials/post.blade.php
@@ -1,10 +1,10 @@
-<article>
+<article class="blogPost">
 	<div class="published"><span class="date">{{ (new DateTime($post->date))->format('F j, Y') }}</span></div>
 	<header>
 		<h1>{{ link_to_post($post) }}</h1>
 		<div class="byline">by {{ $post->author }}</div>
 	</header>
-	<div class="content">
+	<div class="blogPost-body">
 		{{ $post }}
 	</div>
 </article>
