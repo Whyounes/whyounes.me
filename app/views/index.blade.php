a47c94265c2efdf318172fd915b605aa2c280101
Adam Wathan <adam.wathan@gmail.com>
2014-11-10 23:33:37 -0500

    Remove comments link, add margin

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -3,7 +3,6 @@
 @section('content')
 @foreach($posts as $post)
 @include('partials.post')
-<p class="comments-link">{{ link_to_post($post, 'Comments', 'comments') }}</p>
 @endforeach
 {{ $posts->links() }}
 @stop
