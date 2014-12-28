0de90af0d23dc6d42d1416e4ed7d0e35a498f467
Adam Wathan <adam.wathan@gmail.com>
2014-12-28 12:42:21 -0500

    Fix mistakes in about page

--- app/views/about.blade.php
+++ app/views/about.blade.php
@@ -3,9 +3,9 @@
 @section('content')
 <div class="contentSection">
     <h1 class="contentSection-heading">About</h1>
-    <p>I build web applications at <a href="http://tighten.co">Tighten Co</a> in Waterloo by day, and <a href="http://www.youtube.com/watch?v=2-J-DCbsQzo">lift heavy things competitively</a> by night.</p>
+    <p>I build web applications at <a href="http://tighten.co">Tighten Co.</a> by day, and <a href="http://www.youtube.com/watch?v=2-J-DCbsQzo">lift heavy things competitively</a> by night.</p>
 
-    <p>I also host the <a href="http://fullstackradio.com">Full Stack Radio</a> podcast, where I talk to other people in software industry about everything from CSS to unit testing.</p>
+    <p>I also host the <a href="http://fullstackradio.com">Full Stack Radio</a> podcast, where I talk to other people in the software industry about everything from CSS to unit testing.</p>
 
     <p>You can see some of my work on <a href="https://github.com/adamwathan">GitHub</a>, follow me on <a href="http://twitter.com/adamwathan">Twitter</a> or email me at <a href="mailto:adam.wathan@gmail.com">adam.wathan@gmail.com</a>.</p>
 </div>
