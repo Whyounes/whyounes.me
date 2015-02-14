64a03fe6f852c6c39e72f61392ccace8e8ea2c35
Adam Wathan <adam.wathan@gmail.com>
2015-02-14 15:56:51 -0500

    Add RSS autodiscovery

--- app/views/_layout.blade.php
+++ app/views/_layout.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="alternate" type="application/rss+xml" title="adamwathan.me RSS Feed" href="/rss">
     <link href='http://fonts.googleapis.com/css?family=Lato:400,700,900|Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
     @stylesheets('application')
     <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/8.4/styles/github.min.css">
